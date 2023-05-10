5dc03a153f0c8614abe6d143592bea35934c57b7
Andrey C. Dos Santos <Andrey-Carlos@outlook.com>
2023-05-10 20:33:59 -0300

    Ajustado para que seja mais interativo

--- cotacao_moeda.py
+++ cotacao_moeda.py
@@ -1,10 +1,17 @@
 import json, requests
 
-origem = "USD"
-destino = "BRL"
+moedas_disponives = ["USD", "EUR", "BRL", "JPY"]
 
-requisicao = requests.get(f"https://economia.awesomeapi.com.br/{origem}-{destino}")
-cotacao = requisicao.json()
+for moeda in moedas_disponives:
+    print(f"-{moeda}")
 
-print(cotacao[0]["name"])
-print(cotacao[0]["bid"])
+origem  = input("Moeda de origem: " ).upper()
+destino = input("Moeda de destino: ").upper()
+
+if origem and destino in moedas_disponives:
+    requisicao = requests.get(f"https://economia.awesomeapi.com.br/{origem}-{destino}")
+    cotacao    = requisicao.json()
+
+    print(f"1 {origem} equivale a {float(cotacao[0]['bid']):.2f} {destino}")
+else:
+    print("Moedas Inválidas")
